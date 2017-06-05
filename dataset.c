d7f77c876980d2e887d976e5b7e69431e3fa8611
Avinash Sonawane <rootkea@gmail.com>
2017-06-05 13:30:57 +0530

    Modify donor/receiver dataset

--- dataset.c
+++ dataset.c
@@ -13,8 +13,9 @@ mongoimport -d organ -c receiverinfo --jsonArray receiver.json
 char * firstName[] = {"Avinash", "Neha", "Sunil", "Swapnil", "Prithvi"};
 char * lastName[] = {"Sharma", "Irnak", "Sonawane", "Dahawad", "Gupta"};
 char * gender[] = {"male", "female"};
-char * hospital[] = {"KEM Hospital Mumbai", "Sahyadri Hospital Pune", "Aditya Birla Hospital Pune"};
+char * hospital[] = {"KEM Hospital Mumbai", "Sahyadri Hospital Pune", "Vardaan Hospital Delhi"};
 char * domain[] = {"gmail.com", "ymail.com", "hotmail.com"};
+char * doctor[] = {"pawan@gmail.com", "mukesh@gmail.com", "prithvi@gmail.com"};
 
 int approved[] = {0, 1};
 int priority[] = {0, 1, 2, 3, 4};
@@ -29,7 +30,7 @@ int main(int argc, char **argv)
 	int start = atoi(argv[2]);				// start from this id
 	int seed = atoi(argv[3]);				// seed value
 //	char username[21] = "aaaaaa";
-	int i, length = 6;
+	int i, length = 6, hack;
 	
 	srandom(seed);
 	for(i = start; i < start + n; i++)
@@ -38,7 +39,7 @@ int main(int argc, char **argv)
 	
 		fprintf(stdout, "\t\"email\" : "); fprintf(stdout, "\"%d@%s\",\n", i, domain[getRandom(sizeof(domain)/sizeof(domain[0]))]);
 		fprintf(stdout, "\t\"password\" : "); fprintf(stdout, "\"%s\",\n", "1234");
-		fprintf(stdout, "\t\"approved\" : "); fprintf(stdout, "\"%d\",\n", approved[getRandom(sizeof(approved)/sizeof(approved[0]))]);
+		fprintf(stdout, "\t\"approved\" : "); fprintf(stdout, "\"%d\",\n", hack = approved[getRandom(sizeof(approved)/sizeof(approved[0]))]);
 		fprintf(stdout, "\t\"priority\" : "); fprintf(stdout, "\"%d\",\n", priority[getRandom(sizeof(priority)/sizeof(priority[0]))]);
 		fprintf(stdout, "\t\"organ\" : "); fprintf(stdout, "\"%d\",\n", organ[getRandom(sizeof(organ)/sizeof(organ[0]))]);
 //		fprintf(stdout, "\t\"info\" : {\n");
@@ -47,10 +48,10 @@ int main(int argc, char **argv)
 		fprintf(stdout, "\t\"lastname\" : "); fprintf(stdout, "\"%s\",\n", lastName[getRandom(sizeof(lastName)/sizeof(lastName[0]))]);
 		fprintf(stdout, "\t\"gender\" : "); fprintf(stdout, "\"%s\",\n", gender[getRandom(sizeof(gender)/sizeof(gender[0]))]);
 		fprintf(stdout, "\t\"blood\" : "); fprintf(stdout, "\"%d\",\n", blood[getRandom(sizeof(blood)/sizeof(blood[0]))]);
-
 //		fprintf(stdout, "\t},\n");
-		fprintf(stdout, "\t\"hospital\" : "); fprintf(stdout, "\"%s\"\n", hospital[getRandom(sizeof(hospital)/sizeof(hospital[0]))]);
-	
+		fprintf(stdout, "\t\"hospital\" : "); fprintf(stdout, "\"%s\",\n", hospital[hack = getRandom(sizeof(hospital)/sizeof(hospital[0]))]);
+		fprintf(stdout, "\t\"Doc\" : "); fprintf(stdout, "\"%s\"\n", doctor[hack]);
+
 		fprintf(stdout, "}\n");
 	}
 	return 0;
